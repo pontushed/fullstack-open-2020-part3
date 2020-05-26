1b9ca31d765248f868a408351b5f7a751aae2faf
Pontus <pontus.hedlund@gmail.com>
2020-05-26 12:44:37 +0300

    Heroku deployment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req, res) {
   const bodyText = JSON.stringify(req.body)
   return bodyText.length === 2 ? '' : bodyText
 })
+app.use(cors())
 app.use(morgan(':method :url :status :response-time ms :body'))
 
 app.use(express.json())
@@ -102,7 +104,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
