acccd8577beb65a06890867234472f6fa5dabb0f
Pontus <pontus.hedlund@gmail.com>
2020-05-27 13:39:16 +0300

    3.20 Validointi

--- models/person.js
+++ models/person.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose
@@ -15,12 +17,17 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
+    minlength: 3,
   },
   number: {
     type: String,
     required: true,
+    minlength: 8,
   },
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
